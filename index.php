3667dc1bb5c057dac4e2440a549ccd0bd0047dfe
changwoo <ep6tri@hotmail.com>
2021-04-10 18:23:33 +0900

    1.6.0 commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Author:            Changwoo
  * Author URI:        https://blog.changwoo.pe.kr
  * Plugin URI:        https://github.com/chwnam/naran-option-editor
- * Version:           1.5.2
+ * Version:           1.6.0
  * Requires PHP:      7.4
  * Requires at least: 5.0
  * Text Domain:       noe
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 const NOE_MAIN = __FILE__;
-const NOE_VERSION = '1.5.2';
+const NOE_VERSION = '1.6.0';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
